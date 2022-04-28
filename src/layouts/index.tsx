a00e99d4573f02c2dd50f506451bef8cda55aa0a
Trung <ductrung4263@gmail.com>
2022-04-28 15:42:04 +0700

    feat: second commit

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -30,3 +30,19 @@ const Layout = (props:LayoutReceive) => {
 }
 
 export default Layout;
+
+
+
+
+
+
+/***
+ * 
+ * 
+  console.log(`Dinner is served. We’re having ${foodArray[0]}, ${foodArray[1]}, ${foodArray[2]}, and ${foodArray[3]}.`)
+
+  console.log(`Dinner is served. We're having ${foodArray[0]}, ${foodArray[1]}, ${foodArray[2]}, and ${foodArray[3]}.`);
+ * 
+ * 
+ * 
+ */
